db2f27a3578902264be9de2ac7fb9ea21980ac9d
Eric St-Pierre <eric@roulezboulez.ca>
2019-01-31 19:43:36 -0500

    Nettoyage des scripts de test

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,6 @@
 module.exports = function(grunt){
     grunt.initConfig({
         clean: [
-            'backstop_data/bitmaps_test*/**', 
-            'backstop_data/html_report*/**', 
-            'backstop_data/ci_report*/**',
             'spress-site/build/**'
         ],
         
@@ -49,17 +46,10 @@ module.exports = function(grunt){
            spress_generate: {
                 command: 'spress-site/vendor/bin/spress site:build --source="spress-site/"'
             },
-            reference: {
-                command: configFile => 'backstop reference --configPath=' + configFile 
-            },
-            test: {
-                command: configFile => 'backstop test --configPath=' + configFile
-            },
             fractal_build: {
                 command: 'fractal build'
             }
         }
-        
     });
     
     grunt.loadNpmTasks('grunt-shell');
@@ -75,50 +65,4 @@ module.exports = function(grunt){
     grunt.registerTask('clean-all', ['clean']);
     grunt.registerTask('generate', ['shell:spress_generate', 'stylus', 'copy:main']);
     grunt.registerTask('deploy', ['shell:spress_generate', 'stylus', 'copy:main', 'copy:deploy']);
-    //grunt.registerTask('generate', ['shell:spress_generate', 'stylus', 'copy', 'shell:fractal_build']);
-    
-//    grunt.registerTask('reference', [
-//        'generate',
-//        'connect:server',
-//        'shell:reference:test/backstop/backstop-pages.json', 
-//        'shell:reference:test/backstop/backstop-posts.json',
-//        'shell:reference:test/backstop/backstop-projets.json'
-//    ]);
-//    grunt.registerTask('reference-pages', [
-//        'generate',
-//        'connect:server',
-//        'shell:reference:test/backstop/backstop-pages.json'
-//    ]);
-//    grunt.registerTask('reference-posts', [
-//        'generate',
-//        'connect:server',
-//        'shell:reference:test/backstop/backstop-posts.json'
-//    ]);
-//    grunt.registerTask('reference-projets', [
-//        'generate',
-//        'connect:server',
-//        'shell:reference:test/backstop/backstop-projets.json'
-//    ]);
-//    grunt.registerTask('test', [
-//        'generate',
-//        'connect:server',
-//        'shell:test:test/backstop/backstop-pages.json', 
-//        'shell:test:test/backstop/backstop-posts.json',
-//        'shell:test:test/backstop/backstop-projets.json'
-//    ]);
-//    grunt.registerTask('test-pages', [
-//        'generate',
-//        'connect:server',
-//        'shell:test:test/backstop/backstop-pages.json' 
-//    ]);
-//    grunt.registerTask('test-posts', [
-//        'generate',
-//        'connect:server',
-//        'shell:test:test/backstop/backstop-posts.json'
-//    ]);
-//    grunt.registerTask('test-projets', [
-//        'generate',
-//        'connect:server',
-//        'shell:test:test/backstop/backstop-projets.json'
-//    ]);
 };
