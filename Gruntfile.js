e80d5970d23c8c0ec88cd846d52c5dd5c7cfb3f3
Eric St-Pierre <eric@roulezboulez.ca>
2019-02-02 13:04:28 -0500

    Modification du script de génération

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,16 +27,9 @@ module.exports = function(grunt){
                     'spress-site/src/assets/css/main.css' : 'spress-site/src/stylesheets/main.styl' 
                 }
             }
-        },
-        
-        shell: {
-           spress_generate: {
-                command: 'spress-site/vendor/bin/spress site:build --source="spress-site/"'
-            }
         }
     });
     
-    grunt.loadNpmTasks('grunt-shell');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-stylus');
     
@@ -44,6 +37,6 @@ module.exports = function(grunt){
         grunt.log.writeln('default Grunt task');
     });
     
-    grunt.registerTask('generate', ['shell:spress_generate', 'stylus', 'copy:main']);
-    grunt.registerTask('deploy', ['shell:spress_generate', 'stylus', 'copy:main', 'copy:deploy']);
+    grunt.registerTask('generate', ['stylus', 'copy:main']);
+    grunt.registerTask('deploy', ['stylus', 'copy:main', 'copy:deploy']);
 };
