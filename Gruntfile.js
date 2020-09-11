bb3f7ca038a65d2b0d0921fe22896b08e0f604bf
SanPeter <stpierre.eric@gmail.com>
2020-09-10 21:36:37 -0400

    create nuxtjs shell site

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,42 +1,54 @@
-module.exports = function(grunt){
-    grunt.initConfig({
-        copy: {
-          main: {
+module.exports = function (grunt) {
+  grunt.initConfig({
+    copy: {
+      main: {
+        expand: true,
+        cwd: 'spress-site/src/assets/',
+        src: '**',
+        dest: 'spress-site/build/'
+      },
+      deploy: {
+        files: [
+          { expand: true, cwd: 'spress-site/build/', src: '**', dest: 'docs/' },
+          {
             expand: true,
-            cwd: 'spress-site/src/assets/',
+            cwd: 'spress-site/build/',
             src: '**',
-            dest: 'spress-site/build/'
+            dest: 'static/'
           },
-          deploy: {
-              expand: true,
-              cwd: 'spress-site/build/',
-              src: '**',
-              dest: 'docs/'
+          {
+            expand: true,
+            cwd: 'spress-site/build/',
+            src: 'index.html',
+            dest: 'pages/'
           }
-        },        
-        stylus: {
-            compile: {
-                options: {
-                  'include css': true,
-                  compress: true,
-                  linenos: false,
-                  'resolve url': true,
-                  paths: ['spress-site/src/assets/']
-                },
-                files: {
-                    'spress-site/src/assets/css/main.css' : 'spress-site/src/stylesheets/main.styl' 
-                }
-            }
+        ]
+      }
+    },
+    stylus: {
+      compile: {
+        options: {
+          'include css': true,
+          compress: true,
+          linenos: false,
+          'resolve url': true,
+          paths: ['spress-site/src/assets/']
+        },
+        files: {
+          'spress-site/src/assets/css/main.css':
+            'spress-site/src/stylesheets/main.styl'
         }
-    });
-    
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-stylus');
-    
-    grunt.registerTask('default', function() {
-        grunt.log.writeln('default Grunt task');
-    });
-    
-    grunt.registerTask('generate', ['stylus', 'copy:main']);
-    grunt.registerTask('deploy', ['stylus', 'copy:main', 'copy:deploy']);
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+
+  grunt.registerTask('default', function () {
+    grunt.log.writeln('default Grunt task');
+  });
+
+  grunt.registerTask('generate', ['stylus', 'copy:main']);
+  grunt.registerTask('deploy', ['stylus', 'copy:main', 'copy:deploy']);
 };
