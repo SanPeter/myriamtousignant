4d72076d9e705c2bd08ad40c60db6e5eed0986c0
SanPeter <stpierre.eric@gmail.com>
2020-09-12 20:36:19 -0400

    Add styles

---
+++ gatsby-site/gatsby-browser.js
@@ -0,0 +1,2 @@
+import "./src/styles/main.css"
+import "./src/styles/infinitythemetrust/css/prettyPhoto.css?ver=3.1.2"
--- gatsby-site/src/pages/404.js
+++ gatsby-site/src/pages/404.js
@@ -1,5 +1,5 @@
 import React from "react"
 
 export default function Home() {
-  return <h1>Presse</h1>
+  return <div>404 - Not found</div>
 }
